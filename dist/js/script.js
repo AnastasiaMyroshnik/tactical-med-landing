a4c6b428d820bc0322c9c19547d377b19b31e792
Анастасія Мирошник <anastasiamirosnik@MacBook-Air-Anastasia.local>
2024-01-21 10:06:47 +0200

    before modal

---
+++ dist/js/script.js
@@ -0,0 +1,74 @@
+const scrolling = (upSelector) => {
+    const upElem = document.querySelector(upSelector);
+
+    window.addEventListener('scroll', () => {
+        if (document.documentElement.scrollTop > 550) {
+            upElem.classList.add('animated', 'fadeIn');
+            upElem.classList.remove('fadeOut');
+        } else {
+            upElem.classList.add('fadeOut');
+            upElem.classList.remove('fadeIn');
+        }
+    });
+
+    let links = document.querySelectorAll('[href^="#"]'),
+        speed = 0.3;
+    
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            let widthTop = document.documentElement.scrollTop,
+                hash = this.hash,
+                toBlock = document.querySelector(hash).getBoundingClientRect().top,
+                start = null;
+
+            requestAnimationFrame(step);
+
+            function step(time) {
+                if (start === null) {
+                    start = time;
+                }
+
+                let progress = time - start,
+                    r = (toBlock < 0 ? Math.max(widthTop - progress/speed, widthTop + toBlock) : Math.min(widthTop + progress/speed, widthTop + toBlock));
+
+                    document.documentElement.scrollTo(0, r);
+
+                if (r != widthTop + toBlock) {
+                    requestAnimationFrame(step);
+                } else {
+                    location.hash = hash;
+                }
+            }
+        });
+    });
+}
+
+const burger = (burgerSelector, burgerElementsSelector, menuSelector, menuLinkSelector) => {
+
+    const burgerBtn = document.querySelector(burgerSelector);
+    const burgerElements = document.querySelectorAll(burgerElementsSelector);
+    const menuElement = document.querySelector(menuSelector);
+    const menuElements = document.querySelectorAll(menuLinkSelector);
+    const bodyElement = document.querySelector('body');
+    let styleTop = getComputedStyle(menuElement).top.slice(0, -2);
+    const screenHeight = (document.documentElement.clientHeight - styleTop) + 'px';
+ 
+    menuElement.style.height = screenHeight;
+    const toggleClasses = () => {
+        menuElement.classList.toggle('burger__menu--active');
+        bodyElement.classList.toggle('noscroll');
+        burgerElements.forEach( (item) => {
+            item.classList.toggle('burger-elem--transformed')
+        })
+    }
+
+    burgerBtn.addEventListener('click', () => toggleClasses());
+    menuElements.forEach( (item) => {
+        item.addEventListener('click', () => toggleClasses())
+    })
+}
+
+scrolling('.pageup-arrow');
+burger('.burger', '.burger-elem', '.header__menu-mobile', '.header__menu-link');
---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
+
+gulp.task('server', function () {
+
+  browserSync({
+    server: {
+      baseDir: "dist"
+    }
+  });
+
+  gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function () {
+  return gulp.src("src/sass/**/*.+(scss|sass)")
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(rename({ suffix: '.min', prefix: '' }))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function () {
+  gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+  gulp.watch("src/js/**/*.js", gulp.parallel('scripts'));
+  gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+});
+
+gulp.task('html', function () {
+  return gulp.src("src/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("dist/"))
+});
+
+gulp.task('scripts', function () {
+  return gulp.src("src/js/**/*.js")
+    .pipe(gulp.dest("dist/js"))
+});
+
+gulp.task('images', function () {
+  return gulp.src("src/img/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/img"))
+});
+
+gulp.task('icons', function () {
+  return gulp.src("src/icons/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/icons"))
+});
+
+gulp.task('default', gulp.parallel('server', 'styles', 'watch', 'html', 'scripts', 'images', 'icons'));
