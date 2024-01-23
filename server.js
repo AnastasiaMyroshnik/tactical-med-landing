70d9ef04bedef9e1118e97a5dffc909e01d2152d
Анастасія Мирошник <anastasiamirosnik@MacBook-Air-Anastasia.local>
2024-01-23 20:00:57 +0200

    add privacy

---
+++ server.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const express = require('express');
+const { Telegraf } = require('telegraf');
+require('dotenv').config();
+
+const DIST_DIR = path.join(__dirname, 'dist');
+const PORT = process.env.SERVER_PORT;
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+const app = express();
+const bot = new Telegraf(BOT_TOKEN)
+
+app.use(express.static(DIST_DIR));
+
+app.get('/handleForm', (req, res) => {
+  console.log(req.query);
+  const message = '🤩 Нова заявка на курс!\n\n'
+    + `Ім\'я: ${req.query.name}\n`
+    + `Телефон: ${req.query.phone}\n`
+    + `Телеграм: ${req.query.telegram}\n`;
+  bot.telegram.sendMessage(987200314, message);
+  res.status(200).redirect('/');
+})
+
+app.get('/health', (req, res) => {
+  res.send('здарова');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(DIST_DIR, 'index.html'));
+});
+
+app.listen(PORT);
